5fb107ca7abbb691a31ce9be9ab5c2ef3e57f6e4
Sarah Herzog <sarah.ann.herzog@gmail.com>
2013-01-30 20:01:59 +0000

    Imports the outline for SystemManager class

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -74,13 +74,33 @@
 // |----------------------------------------------------------------------------|
 // |								Includes									|
 // |----------------------------------------------------------------------------|
+#include "SystemManager.h"
 
 
-//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
-//{
-//	bool result;
-//	
-//
-//
-//	return 0;
-//}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+{
+	SystemManager* system;
+	bool result;
+	
+	
+	// Create the system object.
+	system = new SystemManager;
+	if(!system)
+	{
+		return 0;
+	}
+
+	// Initialize and run the system object.
+	result = system->Initialize();
+	if(result)
+	{
+		System->Run();
+	}
+
+	// Shutdown and release the system object.
+	system->Shutdown();
+	delete system;
+	system = 0;
+
+	return 0;
+}
---
+++ Engine/SystemManager.h
@@ -0,0 +1,108 @@
+// Solar Exploration Sim
+// Developed a coursework for Abertay University
+// Based on tutorials from http://www.rastertek.com
+// Copyright Sarah Herzog, 2013, all rights reserved.
+//
+// SystemManager
+//		Contains management classes for game objects, graphics, and input.
+//		Runs main game loop and calls sub loops.
+#pragma once
+
+// TODO: Make this singleton
+
+
+// |----------------------------------------------------------------------------|
+// |						Pre-Processing Directives							|
+// |----------------------------------------------------------------------------|
+#define WIN32_LEAN_AND_MEAN
+
+
+// |----------------------------------------------------------------------------|
+// |								Includes									|
+// |----------------------------------------------------------------------------|
+#include <windows.h>
+//#include "inputclass.h"
+//#include "graphicsclass.h"
+//#include "fpsclass.h"
+//#include "cpuclass.h"
+//#include "timerclass.h"
+//#include "positionclass.h"
+//#include "soundclass.h"
+
+
+// |----------------------------------------------------------------------------|
+// |							Class: SystemClass								|
+// |----------------------------------------------------------------------------|
+class SystemManager
+{
+public:
+
+	//|-------------------------------Public Functions--------------------------|
+
+	// Constructors and Destructors
+	SystemManager();
+	SystemManager(const SystemManager&);
+	~SystemManager();
+
+	// Initialize all data members
+	bool Initialize();
+
+	// Release all data members
+	void Shutdown();
+
+	// Run the game (contains main game loop)
+	void Run();
+
+	// Handles system messages
+	LRESULT CALLBACK MessageHandler(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
+
+private:
+
+	//|-------------------------------Private Functions-------------------------|
+
+	// Processes each frame (calles input, game, render frame functions)
+	bool Frame();
+
+	// Performs Windows initialization tasks
+	void InitializeWindows(int& screenWidth, int& screenHeight);
+
+	// Performs Windows shutdown tasks
+	void ShutdownWindows();
+
+private:
+
+	// Windows parameters
+	LPCWSTR m_applicationName;
+	HINSTANCE m_hinstance;
+	HWND m_hwnd;
+
+	// Input handler
+	//InputClass* m_Input;
+
+	// Graphics handler
+	//GraphicsClass* m_Graphics;	
+	
+	// Timing classes
+	//FpsClass* m_Fps;
+	//CpuClass* m_Cpu;
+	//TimerClass* m_Timer;
+
+	// Player position
+	//PositionClass* m_Position;
+
+	// Sound
+	//SoundClass* m_Sound;
+};
+
+
+// |----------------------------------------------------------------------------|
+// |								 Globals									|
+// |----------------------------------------------------------------------------|
+static SystemManager* ApplicationHandle = 0;
+
+
+// |----------------------------------------------------------------------------|
+// |							Function Prototypes								|
+// |----------------------------------------------------------------------------|
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT umessage, WPARAM wparam, LPARAM lparam);
+
